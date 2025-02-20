b799df0ec7c1e0a57db17ff3bd2a42481b511ffb
birdiebairdie <birdiebairdie@gmail.com>
2025-02-20 12:46:38 -0500

    added player class and rotation

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+clock = pygame.time.Clock()
+dt = 0
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
@@ -11,12 +14,21 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+
+	player = Player(x, y)
 
 	while True:
 		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
+			if event.type == pygame.QUIT:
+				return
+			
+		dt = clock.tick(60)/1000
+
 		screen.fill("black")
+		player.update(dt)
+		player.draw(screen)
 		pygame.display.flip()
 
 if __name__ == "__main__":
