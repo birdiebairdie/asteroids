f288f28be5b3d448bf7ada71f2969825d1d237e6
birdiebairdie <birdiebairdie@gmail.com>
2025-02-28 13:33:14 -0500

    changed multiple points for readability, added asteroid destruction from shots

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
-clock = pygame.time.Clock()
-dt = 0
+
 from constants import *
-from player import *
-from asteroidfield import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	x = SCREEN_WIDTH / 2
-	y = SCREEN_HEIGHT / 2
 
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
@@ -26,29 +27,39 @@ def main():
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = (updatable)
-	Shot.containers = (updatable, drawable)
+	Shot.containers = (shots, updatable, drawable)
 
-	player = Player(x, y)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	asteroid_field = AsteroidField()
+
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 			
-		dt = clock.tick(60)/1000
+		updatable.update(dt)
+
+		for asteroid in asteroids:
+			if player.collides_with(asteroid):
+				print("Game over!")
+				sys.exit()
+			
+			for shot in shots:
+				if shot.collides_with(asteroid):
+					print("Collision detected!")
+					asteroid.split()
+					shot.kill()
 
 		screen.fill("black")
-		updatable.update(dt)
-		player.timer -= dt
+		
+		player.shoot_timer -= dt
 		for object in drawable:
 			object.draw(screen)
 		pygame.display.flip()
 
-		for asteroid in asteroids:
-			if player.is_collision(asteroid):
-				print("Game over!")
-				raise SystemExit
+		dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
